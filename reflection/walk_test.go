d15eb18dc8915f6c946919f31ef1da153a9f35c5
Matt Ralph <mralph@equalexperts.com>
2019-03-22 16:43:55 +0000

    Completed reflection

---
+++ reflection/walk_test.go
@@ -0,0 +1,97 @@
+package reflection
+
+import (
+	"reflect"
+	"testing"
+)
+
+type Person struct {
+	Name    string
+	Profile Profile
+}
+
+type Profile struct {
+	Age  int
+	City string
+}
+
+func TestWalk(t *testing.T) {
+	cases := []struct {
+		Name          string
+		Input         interface{}
+		ExpectedCalls []string
+	}{
+		{
+			"Struct with one string field",
+			struct {
+				Name string
+			}{"Chris"},
+			[]string{"Chris"},
+		},
+		{
+			"Struct with two string field",
+			struct {
+				Name string
+				City string
+			}{"Chris", "London"},
+			[]string{"Chris", "London"},
+		},
+		{
+			"Struct with non string field",
+			struct {
+				Name string
+				Age  int
+			}{"Chris", 28},
+			[]string{"Chris"},
+		},
+		{
+			"Nested fields",
+			Person{
+				"Chris",
+				Profile{
+					Age:  24,
+					City: "Manchester",
+				},
+			},
+			[]string{"Chris", "Manchester"},
+		},
+		{
+			"Nested fields given by pointer",
+			&Person{
+				"Chris",
+				Profile{
+					Age:  24,
+					City: "Manchester",
+				},
+			},
+			[]string{"Chris", "Manchester"},
+		},
+		{
+			"Slices",
+			[]Profile{
+				{33, "London"},
+				{34, "Reykjavik"},
+			},
+			[]string{"London", "Reykjavik"},
+		},
+		{
+			"Arrays",
+			[2]Profile{
+				{33, "London"},
+				{34, "Reykjavik"},
+			},
+			[]string{"London", "Reykjavik"},
+		},
+	}
+	for _, test := range cases {
+		t.Run(test.Name, func(t *testing.T) {
+			var got []string
+			walk(test.Input, func(input string) {
+				got = append(got, input)
+			})
+			if !reflect.DeepEqual(got, test.ExpectedCalls) {
+				t.Errorf("got '%v', expected '%v' given %v", got, test.ExpectedCalls, test.Input)
+			}
+		})
+	}
+}
